b6933d0eac71d39f496d9e1d01c7e18308bd668c
Edwin Rugoogamu <rugoogamuedwin5@gmail.com>
2023-07-12 23:58:35 +0300

    cucumber calculator

--- calculator.js
+++ calculator.js
@@ -1,21 +1,22 @@
-var reporter = require('cucumber-html-reporter');
+// var reporter = require('cucumber-html-reporter');
 
-var options = {
-        theme: 'bootstrap',
-        jsonFile: 'reports/cucumber_report.json',
-        output: 'reports/cucumber_report.html',
-        reportSuiteAsScenarios: true,
-        scenarioTimestamp: true,
-        launchReport: true,
-        metadata: {
-            "App Version":"0.3.2",
-            "Test Environment": "STAGING",
-            "Browser": "Chrome  54.0.2840.98",
-            "Platform": "Windows 10",
-            "Parallel": "Scenarios",
-            "Executed": "Remote"
-        },
-        failedSummaryReport: true,
-    };
 
-    reporter.generate(options);
+// var options = {
+//         theme: 'bootstrap',
+//         jsonFile:'cucumber.json',
+//         output:'cucumber_report.html',
+//         reportSuiteAsScenarios: true,
+//         scenarioTimestamp: true,
+//         launchReport: true,
+//         metadata: {
+//             "App Version":"0.3.2",
+//             "Test Environment": "STAGING",
+//             "Browser": "Chrome  54.0.2840.98",
+//             "Platform": "Windows 10",
+//             "Parallel": "Scenarios",
+//             "Executed": "Remote"
+//         },
+//         failedSummaryReport: true,
+//     };
+
+//     reporter.generate(options);
